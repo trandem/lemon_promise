919805f67409b097d2ddaf7b3b346f3bf53c0196
trandem <dem.tran@dnse.com.vn>
2024-08-07 16:06:06 +0700

    feat: all promise

--- lemon_app.go
+++ lemon_app.go
@@ -7,6 +7,46 @@ import (
 )
 
 func main() {
+	ex1()
+	ex2()
+	ex3()
+}
+func ex3() {
+	p1 := lemon_promise.NewCompletablePromise()
+	p2 := lemon_promise.NewCompletablePromise()
+	p3 := lemon_promise.NewSimplePromise("abc")
+
+	lemon_promise.NewJoinPromise(p1, p2, p3).Done(func(r any) {
+		joinRs := r.(*lemon_promise.JoinResult)
+		listRs := joinRs.Results
+		fmt.Println(listRs[0])
+		fmt.Println(listRs[1])
+		fmt.Println(listRs[2])
+	}).
+		Fail(func(e error) {
+			fmt.Println(e)
+		})
+	p2.Resolve("2")
+	p1.Reject(errors.New("test"))
+}
+
+func ex2() {
+	p1 := lemon_promise.NewCompletablePromise()
+	p2 := lemon_promise.NewCompletablePromise()
+	p3 := lemon_promise.NewSimplePromise("abc")
+
+	lemon_promise.NewJoinPromise(p1, p2, p3).Done(func(r any) {
+		joinRs := r.(*lemon_promise.JoinResult)
+		listRs := joinRs.Results
+		fmt.Println(listRs[0])
+		fmt.Println(listRs[1])
+		fmt.Println(listRs[2])
+	})
+	p2.Resolve("2")
+	p1.Resolve(1)
+}
+
+func ex1() {
 	c := lemon_promise.NewCompletablePromise()
 
 	lemon_promise.NewSimplePromise("444").
--- lemon_promise/join_promise.go
+++ lemon_promise/join_promise.go
@@ -1,4 +1,84 @@
 package lemon_promise
 
+import "sync"
+
 type JoinPromise struct {
+	result       any
+	err          error
+	doneCallBack DoneCallback
+	failCallBack FailCallBack
+	mutex        *sync.Mutex
+}
+
+type JoinResult struct {
+	Results []any
+}
+
+func NewJoinPromise(promises ...Promise) *JoinPromise {
+	mutex := &sync.Mutex{}
+	listResult := make([]any, len(promises))
+
+	jp := JoinPromise{mutex: mutex}
+	c := 0
+	for i, p := range promises {
+		p.Done(func(r any) {
+			mutex.Lock()
+			listResult[i] = r
+			c++
+			mutex.Unlock()
+			if c == len(promises) {
+				jp.Resolve(&JoinResult{Results: listResult})
+			}
+		}).Fail(func(e error) {
+			jp.Reject(e)
+		})
+	}
+
+	return &jp
+}
+
+func (j *JoinPromise) Resolve(result any) {
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+	j.result = result
+	if j.doneCallBack != nil {
+		j.doneCallBack(j.result)
+	}
+}
+
+func (j *JoinPromise) Reject(err error) {
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+	j.err = err
+	if j.failCallBack != nil {
+		j.failCallBack(err)
+	}
+}
+
+func (j *JoinPromise) Then(callback PipeDoneCallback) Promise {
+	return NewPipePromise(j, callback)
+}
+
+func (j *JoinPromise) Done(callback DoneCallback) Promise {
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+	j.doneCallBack = callback
+	if j.result != nil {
+		j.doneCallBack(j.result)
+	}
+	return j
+}
+
+func (j *JoinPromise) Fail(callback FailCallBack) Promise {
+	j.mutex.Lock()
+	defer j.mutex.Unlock()
+	j.failCallBack = callback
+	if j.err != nil {
+		j.failCallBack(j.err)
+	}
+	return j
+}
+
+func (j *JoinPromise) Get() any {
+	return j.result
 }
